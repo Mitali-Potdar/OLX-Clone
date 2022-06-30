7eaeb0ebc200ffe21e4a024a652219ba8c25d4a5
Mitali Potdar <mitalipotdar1@gmail.com>
2022-07-01 00:06:43 +0530

    Register User

---
+++ my-app/controllers/userCtrl.js
@@ -0,0 +1,61 @@
+const Users = require('../models/userModel')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const sendMail = require('./sendMail')
+
+const {CLIENT_URL} = process.env
+
+const userCtrl = {
+    register: async (req, res) => {
+        try {
+            const {name, email, password} = req.body
+            
+            if(!name || !email || !password)
+                return res.status(400).json({msg: "Please fill in all fields."})
+
+            if(!validateEmail(email))
+                return res.status(400).json({msg: "Invalid emails."})
+
+            const user = await Users.findOne({email})
+            if(user) return res.status(400).json({msg: "This email already exists."})
+
+            if(password.length < 6)
+                return res.status(400).json({msg: "Password must be at least 6 characters."})
+
+            const passwordHash = await bcrypt.hash(password, 12)
+
+            const newUser = {
+                name, email, password: passwordHash
+            }
+
+            const activation_token = createActivationToken(newUser)
+
+            const url = `${CLIENT_URL}/user/activate/${activation_token}`
+            //sendMail(email, url, "Verify your email address")
+
+
+            res.json({msg: "Register Success! Please activate your email to start."})
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    }
+}
+
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
+const createActivationToken = (payload) => {
+    return jwt.sign(payload, process.env.ACTIVATION_TOKEN_SECRET, {expiresIn: '5m'})
+}
+
+const createAccessToken = (payload) => {
+    return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '15m'})
+}
+
+const createRefreshToken = (payload) => {
+    return jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, {expiresIn: '7d'})
+}
+
+module.exports = userCtrl
---
+++ my-app/routes/userRouter.js
@@ -0,0 +1,6 @@
+const router = require('express').Router()
+const userCtrl = require('../controllers/userCtrl')
+
+router.post('/register', userCtrl.register)
+
+module.exports = router;
