e319c3a4c2af09adc6cd050adb58ced3a2d9e73e
Mitali Potdar <mitalipotdar1@gmail.com>
2022-07-01 02:08:19 +0530

    Activation Email

--- my-app/controllers/userCtrl.js
+++ my-app/controllers/userCtrl.js
@@ -31,14 +31,37 @@ const userCtrl = {
             const activation_token = createActivationToken(newUser)
 
             const url = `${CLIENT_URL}/user/activate/${activation_token}`
-            //sendMail(email, url, "Verify your email address")
+            sendMail(email, url, "Verify your email address")
 
 
             res.json({msg: "Register Success! Please activate your email to start."})
         } catch (err) {
             return res.status(500).json({msg: err.message})
         }
-    }
+    },
+
+    activateEmail: async (req, res) => {
+        try {
+            const {activation_token} = req.body
+            const user = jwt.verify(activation_token, process.env.ACTIVATION_TOKEN_SECRET)
+
+            const {name, email, password} = user
+
+            const check = await Users.findOne({email})
+            if(check) return res.status(400).json({msg:"This email already exists."})
+
+            const newUser = new Users({
+                name, email, password
+            })
+
+            await newUser.save()
+
+            res.json({msg: "Account has been activated!"})
+
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
 }
 
 function validateEmail(email) {
