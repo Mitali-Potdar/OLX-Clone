4b8c1fc4bf0265686125d25b5413f9c623f39daa
Mitali Potdar <mitalipotdar1@gmail.com>
2022-07-02 11:02:15 +0530

    git-ignore

--- my-app/middleware/uploadImage.js
+++ my-app/middleware/uploadImage.js
@@ -6,7 +6,7 @@ module.exports = async function(req, res, next) {
             return res.status(400).json({msg: "No files were uploaded."})
             
         const file = req.files.file;
-        //console.log(file)
+        
         if(file.size > 1024 * 1024){
             removeTmp(file.tempFilePath)
             return res.status(400).json({msg: "Size too large."})
